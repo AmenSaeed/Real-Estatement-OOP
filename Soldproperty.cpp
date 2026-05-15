33232c4c8a926fe3af15fb51ac1b8993396f6149
Amen Saeed <amnsaidd@gmail.com>
2026-05-15 23:19:25 +0300

    Add color setting functionality to display method

--- Soldproperty.cpp
+++ Soldproperty.cpp
@@ -9,10 +9,17 @@ string Soldproperty::get_date() { return date; }
 double Soldproperty::get_Price() { return price; }
 string Soldproperty::get_soldby() { return soldby; }
 
+void Soldproperty::setColor(int color) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, color);
+}
+
 void Soldproperty::display() {
+    setColor(11);
     cout << "Type: " << type << endl;
     cout << "Date: " << date << endl;
     cout << "Price: " << price << endl;
     cout << "sold by: " << soldby << endl;
+    setColor(15);
     cout << "------------------------------------" << endl;
 }
