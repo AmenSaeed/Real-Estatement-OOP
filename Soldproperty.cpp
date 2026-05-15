1c173a0bb7d72db61486a7b652a4585c8f862ef9
MohamedAdel-cmd <mohamedadel95568w@gmail.com>
2026-05-15 19:25:02 +0300

    Sold Property classes

---
+++ Soldproperty.cpp
@@ -0,0 +1,18 @@
+#include "Soldproperty.h"
+
+Soldproperty::Soldproperty(string ty, string da, double pr, string sb) {
+    type = ty; date = da; price = pr; soldby = sb;
+}
+
+string Soldproperty::get_type() { return type; }
+string Soldproperty::get_date() { return date; }
+double Soldproperty::get_Price() { return price; }
+string Soldproperty::get_soldby() { return soldby; }
+
+void Soldproperty::display() {
+    cout << "Type: " << type << endl;
+    cout << "Date: " << date << endl;
+    cout << "Price: " << price << endl;
+    cout << "sold by: " << soldby << endl;
+    cout << "------------------------------------" << endl;
+}
---
+++ Soldproperty.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Soldproperty {
+private:
+    string type;
+    string date;
+    double price;
+    string soldby;
+
+public:
+    Soldproperty(string ty, string da, double pr, string sb);
+    string get_type();
+    string get_date();
+    double get_Price();
+    string get_soldby();
+    void display();
+};
