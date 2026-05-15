3944af0b35af473d879c9ca0e34bc296e2480251
Amen Saeed <amnsaidd@gmail.com>
2026-05-15 23:13:18 +0300

    Color updated

--- Villa.cpp
+++ Villa.cpp
@@ -7,13 +7,20 @@ Villa::Villa(string ad, int f, int ar, int r, bool gard, bool grg, double prc) :
 int Villa::get_floors() { return floors; }
 int Villa::get_rooms() { return rooms; }
 bool Villa::get_hasGarden() { return hasGarden; }
-bool Villa::get_hasGarage() { return hasGarage; }
+bool Villa::get_hasGarage() { return hasGarage; 
+    void Villa::setColor(int color) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, color);
+    }
+ 
 
 void Villa::display() {
     Available::display();
+    setColor(3);
     cout << "Number of floors: " << floors << endl;
     cout << "Number of rooms: " << rooms << endl;
     cout << "Has garden: " << (hasGarden ? "Yes" : "No") << endl;
-    cout << "Has garage: " << (hasGarage ? "Yes" : "No") << endl;
+    cout << "Has garage: " << (hasGarage ? "Yes" : "No") << endl
+    setColor(15);
     cout << "------------------------------------" << endl;
 }
