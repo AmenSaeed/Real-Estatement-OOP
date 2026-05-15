43d1b43a391eac167b33018a402a941db08f7beb
Amen Saeed <amnsaidd@gmail.com>
2026-05-15 23:15:35 +0300

    Color and default constructor

--- Land.cpp
+++ Land.cpp
@@ -10,12 +10,23 @@ Land::Land(string add, double area, string type, bool status, double tp) : Avail
     hasWaterAndElectricity = status;
 }
 
+Land::Land() {
+    landType = "Not Specified";
+    hasWaterAndElectricity = false;
+}
+
 string Land::get_landType() { return landType; }
 bool Land::get_hasWaterAndElectricity() { return hasWaterAndElectricity; }
+
+void Land::setColor(int color) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, color);
+}
 
 void Land::display() {
     Available::display();
     cout << "Land Type: " << landType << endl;
     cout << "Infrastructure Status: " << (hasWaterAndElectricity ? "Available" : "Not Available") << endl;
+    setColor(15);
     cout << "-------------------------------" << endl;
 }
